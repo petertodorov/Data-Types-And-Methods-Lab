74c2805b53ffeb8492c09babeccd087a554ea3be
Peter Todorov <petodorov@gmail.com>
2016-11-01 13:27:41 +0200

    .
    .

---
+++ ExerciseThree/ExerciseThree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ExerciseThree
+{
+    static void Main(string[] args)
+    {
+        decimal numberToEnter = decimal.Parse(Console.ReadLine());
+        decimal sum = 0;
+        for (int i = 0; i < numberToEnter; i++)
+        {
+            decimal temp = decimal.Parse(Console.ReadLine());
+            sum += temp;
+        }
+        Console.WriteLine(sum);
+    }
+}
