9f859fb9c1ed65098eaf300e6e7a88f95748ba18
Peter Todorov <petodorov@gmail.com>
2016-11-01 17:14:09 +0200

    ExerciseSix
    Checked - works

---
+++ ExerciseSix-Triples Of Latin Letters/ExerciseSixTriplesOfLatinLetters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseSix_Triples_Of_Latin_Letters
+{
+    class ExerciseSixTriplesOfLatinLetters
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("a");
+            }
+    }
+}
---
+++ ExerciseSixTripletsLatinLetters/ExerciseSixTripletsLatinLetters/ExerciseSixTripletsLatinLetters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class ExerciseSixTripletsLatinLetters
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int num = 0; num < n; num++)
+        {
+            for (int num1 = 0; num1 < n; num1++)
+            {
+                for (int num2 = 0; num2 < n; num2++)
+                {
+                    Console.Write((char)('a' + num));
+                    Console.Write((char)('a' + num1));
+                    Console.Write((char)('a' + num2));
+                    Console.WriteLine();
+                }
+                
+            }
+            
+        }
+    }
+}
