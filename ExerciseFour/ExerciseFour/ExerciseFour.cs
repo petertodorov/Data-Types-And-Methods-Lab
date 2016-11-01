b85ca848edeb4a76c1d98bf85c9de93b1e5934ee
Peter Todorov <petodorov@gmail.com>
2016-11-01 14:17:17 +0200

    ExerciseFour
    Chechek - works

---
+++ ExerciseFour/ExerciseFour/ExerciseFour.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class ExerciseFour
+    {
+    static void Main()
+    {
+        decimal n = decimal.Parse(Console.ReadLine());
+        decimal p = decimal.Parse(Console.ReadLine());
+
+        Ceiling( n, p);
+    }
+    static void Ceiling(decimal n, decimal p)
+    {
+        decimal temp = Math.Ceiling(n / p);
+        Console.WriteLine(temp);
+    }
+}
+
