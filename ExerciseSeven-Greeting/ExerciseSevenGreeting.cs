40f1c5daad29dd293a49446d006aaf600a759115
Peter Todorov <petodorov@gmail.com>
2016-11-01 17:42:44 +0200

    ExerciseSeven
    Checked - works

---
+++ ExerciseSeven-Greeting/ExerciseSevenGreeting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ExerciseSevenGreeting
+{
+    static void Main(string[] args)
+    {
+        string firstName = Console.ReadLine();
+        string lasName= Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Hello, {0} {1}.You are {2} years old.",firstName,lasName, age);
+        Console.WriteLine($"Hello, {firstName} {lasName}. You are {age} years old.");
+    }
+}
