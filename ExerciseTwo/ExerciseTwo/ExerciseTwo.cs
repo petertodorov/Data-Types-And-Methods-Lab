b51288ea26fa978f011ec0f9cc27a2f0c1a32afb
Peter Todorov <petodorov@gmail.com>
2016-11-01 13:05:35 +0200

    ExerciseTwo
    Cheked-works

---
+++ ExerciseTwo/ExerciseTwo/ExerciseTwo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class ExerciseTwo
+{
+    static void Main(string[] args)
+    {
+        double r = double.Parse(Console.ReadLine());
+        double area = Math.PI * Math.Pow(r, 2);
+        Console.WriteLine("{0:f12}", area);
+    }
+}
