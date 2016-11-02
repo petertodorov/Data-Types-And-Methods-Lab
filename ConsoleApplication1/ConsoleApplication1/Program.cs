c40acd1abe1799f5fcedebf537a812b2351a89d6
Peter Todorov <petodorov@gmail.com>
2016-11-02 11:36:06 +0200

    ExerciseNine
    .

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class ExerciseNineAnimalType
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        AnimalType(name);
+    }
+    static void AnimalType(string name)
+    {
+        switch (name)
+        {
+            case "dog": Console.WriteLine("mammal"); break;
+            case "tortoise": Console.WriteLine("reptile"); break;
+            case "crocodile": Console.WriteLine("reptile"); break;
+            case "snake": Console.WriteLine("reptile"); break;
+            default: Console.WriteLine("Unknown"); break;
+        }
+    }
+}
+
