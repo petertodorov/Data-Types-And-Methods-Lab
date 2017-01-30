838ffc67b1e3285a24f2a539f9450519eeeae7ff
Peter Todorov <petodorov@gmail.com>
2017-01-30 17:20:12 +0200

    Update ExerciseSevenGreeting.cs

--- ExerciseSeven-Greeting/ExerciseSevenGreeting.cs
+++ ExerciseSeven-Greeting/ExerciseSevenGreeting.cs
@@ -10,7 +10,7 @@ class ExerciseSevenGreeting
     {
         string firstName = Console.ReadLine();
         string lasName= Console.ReadLine();
-        int age = int.Parse(Console.ReadLine());
+        decimal age = decimal.Parse(Console.ReadLine());
         Console.WriteLine($"Hello, {firstName} {lasName}. You are {age} years old.");
     }
 }
